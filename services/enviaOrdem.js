4fd5b88a88fe1b369674f09108faff08ca65e662
Lumieregyn <lumieregyn.ai@gmail.com>
2025-05-24 17:26:16 -0300

    Add files via upload

--- services/enviaOrdem.js
+++ services/enviaOrdem.js
@@ -1,21 +1,18 @@
 const fs = require('fs');
-const js2xmlparser = require("js2xmlparser");
 const axios = require('axios');
+const { gerarOrdemCompra } = require('./ocGenerator');
 
 async function enviarOrdemCompraReal(token) {
-  const json = require('../pedido_aprovado.json');
-  const xml = js2xmlparser.parse("ordemCompra", json);
-
-  const form = new URLSearchParams();
-  form.append('token', token);
-  form.append('xml', xml);
-  form.append('formato', 'json');
+  const xml = gerarOrdemCompra();
 
   const response = await axios.post(
-    'https://api.tiny.com.br/api2/ordem.compra.incluir.php',
-    form,
+    'https://api.tiny.com.br/api/v3/purchase-order',
+    xml,
     {
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' }
+      headers: {
+        'Authorization': `Bearer ${token}`,
+        'Content-Type': 'application/xml'
+      }
     }
   );
 
