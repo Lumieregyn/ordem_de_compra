6a16505193bf2a68143fd7554634079718146cf2
Lumieregyn <lumieregyn.ai@gmail.com>
2025-05-24 18:56:14 -0300

    Update enviarOrdem.js

--- services/enviarOrdem.js
+++ services/enviarOrdem.js
@@ -1,16 +1,19 @@
 const axios = require('axios');
 
-async function enviarOrdemCompra(xml, token) {
-  return axios.post(
-    'https://erp.tiny.com.br/public-api/v3/purchase-order',
-    xml,
-    {
-      headers: {
-        'Content-Type': 'application/xml',
-        'Authorization': `Bearer ${token}`
-      }
+async function enviarOrdemCompra(token, xml) {
+  const url = 'https://api.tiny.com.br/api2/pedido.compra.incluir.php';
+
+  const response = await axios.post(url, new URLSearchParams({
+    xml: xml,
+    token: token,
+    formato: 'json'
+  }), {
+    headers: {
+      'Content-Type': 'application/x-www-form-urlencoded'
     }
-  );
+  });
+
+  return response.data;
 }
 
 module.exports = { enviarOrdemCompra };
