ec84b7621f63a152cf2c74a5e619188524028cb2
Lumieregyn <lumieregyn.ai@gmail.com>
2025-07-05 22:21:46 -0300

    Delete ordem_de_compra-main (4)/ordem_de_compra-main directory

--- ordem_de_compra-main (4)/ordem_de_compra-main/rastreamentoPedidoEntregaIntegrado/index.js
+++
@@ -1,7 +0,0 @@
-const { monitorarPedidos } = require('./jobs/agendadorPedidos');
-
-monitorarPedidos().then(() => {
-  console.log('✅ Rastreamento concluído.');
-}).catch(err => {
-  console.error('❌ Erro no rastreamento:', err);
-});
